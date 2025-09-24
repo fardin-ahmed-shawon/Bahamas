79e7e7660e1965454b091af9398dbb251860924d
Fardin Ahmed <fardinahmed7443@gmail.com>
2025-09-24 14:51:13 +0600

    first commit

---
+++ navbar.php
@@ -0,0 +1,18 @@
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<style>
+    .form-control {
+        font-family: "Poppins", serif;
+    }
+</style>
+
+<nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+        <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
+          <a class="navbar-brand brand-logo" href="index.php">
+            <img src="img/boris-logo.png" alt="LOGO">
+          </a>
+          <a class="navbar-brand brand-logo-mini" href="index.php">
+            <img src="assets/images/logo-mini.svg" alt="logo" />
+          </a>
+        </div>
+</nav>
---
+++ sidebar.php
@@ -0,0 +1,41 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+          <ul class="nav">
+
+            <li class="nav-item">
+              <a class="nav-link" href="">
+                <span class="menu-title">Login</span>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="">
+                <span class="menu-title">Generate Registration Form</span>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="">
+                <span class="menu-title">Check Vessel Name</span>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="index.php">
+                <span class="menu-title">Verify Certificate</span>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="">
+                <span class="menu-title">Top Up</span>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="">
+                <span class="menu-title">Pay Invoice</span>
+              </a>
+            </li>
+
+          </ul>
+        </nav>
